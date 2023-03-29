c79165825b2566e70cdc04c307982875943a2f73
hoangph3 <phamhoanghxh1@gmail.com>
2023-03-29 21:44:03 +0700

    using opencv

---
+++ certs/gen.sh
@@ -0,0 +1,1 @@
+openssl req -new -x509 -keyout localhost.pem -out localhost.pem -days 3650 -nodes
--- main.py
+++ main.py
@@ -1,57 +1,37 @@
-from typing import *
-from subprocess import Popen
-from fastapi import FastAPI
-from fastapi.openapi.docs import (
-    get_redoc_html,
-    get_swagger_ui_html,
-    get_swagger_ui_oauth2_redirect_html,
-)
-from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
-from fastapi.templating import Jinja2Templates
-from fastapi import Request
-import uvicorn
+"""Simple https server for development."""
+
+import ssl
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+CERTFILE = './certs/localhost.pem'
 
 
-app = FastAPI(docs_url=None, redoc_url=None, debug=True)
-templates = Jinja2Templates(directory="templates")
-app.mount("/static", StaticFiles(directory="static"), name="static")
+def main():
+    https_server(certfile=CERTFILE)
 
-@app.get("/docs", include_in_schema=False)
-async def custom_swagger_ui_html():
-    return get_swagger_ui_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - Swagger UI",
-        oauth2_redirect_url=app.swagger_ui_oauth2_redirect_url,
-        swagger_js_url="/static/js/swagger-ui-bundle.js",
-        swagger_css_url="/static/css/swagger-ui.css",
-    )
 
-@app.get(app.swagger_ui_oauth2_redirect_url, include_in_schema=False)
-async def swagger_ui_redirect():
-    return get_swagger_ui_oauth2_redirect_html()
+def https_server(*, certfile):
+    print('`https_server()` starts...')
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(CERTFILE)
 
-@app.get("/redoc", include_in_schema=False)
-async def redoc_html():
-    return get_redoc_html(
-        openapi_url=app.openapi_url,
-        title=app.title + " - ReDoc",
-        redoc_js_url="/static/js/redoc.standalone.js",
-    )
+    server_address = ('', 8443)
+    with HTTPServer(server_address, SimpleHTTPRequestHandler) as httpd:
+        httpd.socket = context.wrap_socket(httpd.socket, server_side=True)
+        print_server_info(httpd)
+        try:
+            httpd.serve_forever()
+        except Exception as e:
+            httpd.server_close()
+            raise e
 
-@app.get("/video", response_class=HTMLResponse)
-async def video(request: Request):
-    return templates.TemplateResponse("video.html", {"request": request})
 
-@app.get("/test", response_class=HTMLResponse)
-async def test(request: Request):
-    return templates.TemplateResponse("test.html", {"request": request})
+def print_server_info(server):
+    print(f"""Server info:
+    name: {server.server_name}
+    address: {server.server_address}
+    """)
 
 
 if __name__ == "__main__":
-    Popen(['python3', '-m', 'https_redirect'])
-    uvicorn.run(
-        "main:app", host="0.0.0.0", port=443,
-        ssl_keyfile='certs/key.pem',
-        ssl_certfile='certs/cert.pem'
-    )
+    main()
