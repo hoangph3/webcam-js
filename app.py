e7b580a70635c6cd90bb6d667dd4c9afe428a0f5
hoangph3 <phamhoanghxh1@gmail.com>
2023-04-16 11:52:27 +0700

    update

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 from convert_image import b64_to_array
 import matplotlib.pyplot as plt
+import random
 import os
 
 
@@ -11,11 +12,13 @@ CORS(app)
 @app.route('/api/user/pattern', methods=['POST'])
 def hello_world():
     response = request.get_json()
-    # print(response)
+    print(response)
     plt.imsave("images/frame_{}.jpg".format(len(os.listdir("images"))), b64_to_array(response['image']))
-    return jsonify(
-        message='Success'
-    )
+    return make_response(jsonify({
+        "username": "test",
+        "phone": random.randint(0, 1000),
+        "box": response['box']
+    }), 200)
 
 
 if __name__ == '__main__':
