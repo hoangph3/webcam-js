6fb4b2edf877229b27a9f8341f32bca96304f339
hoangph3 <phamhoanghxh1@gmail.com>
2023-04-20 08:44:47 +0700

    update fullscreen

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import os
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/user/pattern', methods=['POST'])
+@app.route('/api/user/pattern', methods=['PUT'])
 def hello_world():
     response = request.get_json()
     print(response)
-    plt.imsave("images/frame_{}.jpg".format(len(os.listdir("images"))), b64_to_array(response['image']))
+    plt.imsave("images/frame_{}.jpg".format(len(os.listdir("images"))), b64_to_array(response['images'][0]))
     return make_response(jsonify({
         "zcfg_requester_comboname": "test",
         "zcfg_requester_phone_number": random.randint(0, 1000),
